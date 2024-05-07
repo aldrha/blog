9032355c5a48c73660267ac1114e1abaeff8227c
Aldrha <adrha@gmail.com>
2024-05-07 16:43:41 -0400

    segundo comentario

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\File;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        File::deleteDirectory(public_path('storage/posts'));
+        File::makeDirectory(public_path('storage/posts'), 0777, true);
+
+        $this->call(UserSeeder::class);
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+        $this->call(PostSeeder::class);
+    }
+}
