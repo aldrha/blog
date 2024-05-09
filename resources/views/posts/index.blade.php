98b3d1c04ed1ccb4a555a3e3baffe2c652b48c14
Aldrha Petit <aldrha@gmail.com>
2024-05-08 20:42:15 -0400

    primera parte de la vista de listado de post

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+
+    <div class="container bg-red-500">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam doloribus necessitatibus, molestiae, dolores
+        nobis aspernatur consequatur magni commodi facere dolore dolor quibusdam, eveniet aliquid totam. Reprehenderit
+        quod ratione eius magni?
+    </div>
+
+</x-app-layout>
