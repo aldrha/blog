47759d1c934f1c6434319cf8840a91f49a05064c
Aldrha Petit <aldrha@gmail.com>
2024-05-14 16:47:03 -0400

    ajuste vender Image faker y comienzo de listado de post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,14 @@
 <x-app-layout>
 
-    <div class="container bg-red-500">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam doloribus necessitatibus, molestiae, dolores
-        nobis aspernatur consequatur magni commodi facere dolore dolor quibusdam, eveniet aliquid totam. Reprehenderit
-        quod ratione eius magni?
+    <div class="container py-8">
+        <div class="grid grid-cols-3 gap-6">
+            @foreach ($posts as $post)
+                <article class="w-full h-80 bg-cover bg-center @if($loop->first) col-span-2 @endif" style="background-image: url({{Storage::url($post->image->url)}})">  
+                    <div class="w-fyll h-full px-8 flex flex-col justify-center">
+                        asiodoasi</div> 
+                </article>
+            @endforeach
+        </div>
     </div>
 
 </x-app-layout>
