533a2a276d3bba2d1e64e11a5d4eaa4c4098f3fd
Aldrha Petit <aldrha@gmail.com>
2024-05-14 16:48:35 -0400

    ajuste

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
         <div class="grid grid-cols-3 gap-6">
             @foreach ($posts as $post)
                 <article class="w-full h-80 bg-cover bg-center @if($loop->first) col-span-2 @endif" style="background-image: url({{Storage::url($post->image->url)}})">  
-                    <div class="w-fyll h-full px-8 flex flex-col justify-center">
+                    <div class="w-full h-full px-8 flex flex-col justify-center">
                         asiodoasi</div> 
                 </article>
             @endforeach
